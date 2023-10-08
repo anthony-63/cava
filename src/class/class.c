81834028b01a25368af604043773ea82d7961b91
anthony-63 <back.up.email.158@gmail.com>
2023-10-08 13:33:34 -0400

    Version parsing

--- src/class/class.c
+++ src/class/class.c
@@ -6,14 +6,16 @@ CavaClassFile read_class_file(const char* file_name) {
     CavaClassFile class;
 
     class.magic = class_reader_get_u32(reader);
-
+    class.minor_version = class_reader_get_u16(reader);
+    class.major_version = class_reader_get_u16(reader);
+    
     if(class.magic != 0xcafebabe) {
         fprintf(stderr, "Invalid ClassFile magic: %08x\n", class.magic);
         exit(-1);
     }
 
     printf("---------- CLASS INFO ----------\n");
-    printf("MAGIC: %08x", class.magic);
-
+    printf("MAGIC: %08x\n", class.magic);
+    printf("VERSION: %d.%d\n", class.major_version, class.minor_version);
     return class;
 }
